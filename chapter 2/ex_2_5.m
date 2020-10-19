66d389d970fde6c64af4d863a7fa70ac4f323411
mikalaki <mikalakis@hotmail.gr>
2020-10-19 03:10:15 +0300

    fixed exercises' 2.5 empty file

--- chapter 2/ex_2_5.m
+++ chapter 2/ex_2_5.m
@@ -15,9 +15,9 @@ sigma = 0.1;
 
 currLowerDiameterLimit = 3.9;
 
-%the probability a rail to get destroyed:
+%the probability a rail get destroyed:
 prob1 = normcdf(currLowerDiameterLimit,mu,sigma);
-fprintf("The probability a rail to get destroyed is %f or %.2f%% \n",prob1,prob1*100);
+fprintf("The probability a rail get destroyed is %f or %.2f%% \n",prob1,prob1*100);
 
 %the lower limit in order only the 1% of the rails to get destroyed
 newLowerDiameterLimit = norminv(0.01,mu,sigma);
